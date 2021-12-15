f5a5406c1ca4d642c550f876f53e70544103bd6e
Anas <anasakzaz23@gmail.com>
2021-12-15 15:34:59 +0100

    second commit

---
+++ src/Components/Body/index.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function Body() {
+    return (
+        <div className='Container'>
+            <div className='Wrapper'>
+                <h1>Welcome to the body part</h1>
+
+                <section>Hello evry body</section>
+
+                <table className='pull-right'>
+                    <tr>Nom: Anas AKZAZ</tr>
+                    <tr>Filliere: DevOps</tr>
+                    <tr>Age: 21 ans</tr>
+                    <img width="150px" src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" />
+                </table>
+            </div>
+        </div>
+    )
+}
+
+export default Body
